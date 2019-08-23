1a62d894c982021ae3b884b3c0a80dd7726a3f57
meginks <meganjones.dev@gmail.com>
2019-08-23 10:21:59 -0500

    finished letter to readers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import {Header, Image, Segment, Container} from 'semantic-ui-react';
+import SV1 from './components/images/SV1.jpg'
+import LetterToReaders from './components/LetterToReaders'
 
 function App() {
   return (
-    <div>
-     Vanzetti and Sacco
-    </div>
+      <Container>
+        <div className="heading-name">
+        <Header>
+         <h1 className="heading-name">Vanzetti and Sacco</h1>      
+        </Header>
+        </div>
+     <LetterToReaders />
+
+     </Container>
   );
 }
 
